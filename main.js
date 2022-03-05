4bdba7d19ec90888fb34952c821f3bbcfa5ab1d3
utkur <utkur@bk.ru>
2022-03-06 00:07:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function Stepen() {
+    let x = +prompt("Введите число: "),
+        y = +prompt("Введите степень: "),
+        
+      
+        z = x ** y;
+
+   
+
+    alert(z);
+    
+};
+Stepen();
+
+
+
+
+
+for (let i = 0; i < 5; i++) {
+    let line = ''
+    for(let j = 0; j < i; j++) {
+      line += '-'
+    }
+    console.log(`${line}*`)
+  }
+
+
+
+
+
+
